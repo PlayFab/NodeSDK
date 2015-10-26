20bc4b7a9627468633f7f7e58ace052147ad98bd
pgilmorepf <paul@playfab.com>
2015-10-26 10:55:41 -0700

    Updating all SDKs after changes to API_Specs

--- PlayFabTesting/PlayFab.js
+++ PlayFabTesting/PlayFab.js
@@ -1,8 +1,8 @@
 var url = require('url');
 var https = require('https');
 
-exports.sdk_version = "0.1.151019";
-exports.api_version = "1.7.20151019";
+exports.sdk_version = "0.2.151026";
+exports.api_version = "1.8.20151026";
 
 exports.settings = {
     useDevEnv: false,
