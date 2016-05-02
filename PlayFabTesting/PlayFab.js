84ed21399335e251962bf37a0cda874ba15c12bc
Paul Gilmore <paul+public@playfab.com>
2016-05-02 10:31:53 -0700

    https://api.playfab.com/releaseNotes#160502 (#66)
    * Automated build from Jenkins
    
    * Automated build from Jenkins
    
    * Automated build from Jenkins
    
    * Automated build from Jenkins

--- PlayFabTesting/PlayFab.js
+++ PlayFabTesting/PlayFab.js
@@ -1,7 +1,7 @@
 var url = require("url");
 var https = require("https");
 
-exports.sdk_version = "0.17.160425";
+exports.sdk_version = "0.18.160502";
 
 var settings = exports.settings = {
     useDevEnv: false,
