02c8ffefb59b18bed6f0daa49f9bb3d279ffc79f
sinitreo <sinitreo@gmail.com>
2017-11-15 05:25:50 +0500

    - minor fixes

--- PlayFabSdk/tests/run.bat
+++ PlayFabSdk/tests/run.bat
@@ -1,5 +1,5 @@
 cd tests
 set cd=%cd%
-IF "%PF_TEST_TITLE_DATA_JSON%"=="" set PF_TEST_TITLE_DATA_JSON=./testTitleData.js
+IF "%PF_TEST_TITLE_DATA_JSON%"=="" set PF_TEST_TITLE_DATA_JSON=./testTitleData.json
 echo %cd%
 nodeunit tests.js --reporter %cd%/reporter.js
