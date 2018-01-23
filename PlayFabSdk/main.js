7653aa9340dff7e58b4f11146a0e7dd946a8e74e
Paul Gilmore <paul@playfab.com>
2018-01-23 14:26:54 -0800

    Cleanup
    Cleaning up some unnecessary leftovers from the 2.0 upgrade that were left behind.

--- PlayFabSdk/main.js
+++
@@ -1,7 +0,0 @@
-// main.js not intended for public consumption, it just loads all the required files for npm package
-
-var PlayFab = require('./Scripts/PlayFab/PlayFab.js');
-var PlayFabAdmin = require('./Scripts/PlayFab/PlayFabAdmin.js');
-var PlayFabMatchmaker = require('./Scripts/PlayFab/PlayFabMatchmaker.js');
-var PlayFabClient = require('./Scripts/PlayFab/PlayFabClient.js');
-var PlayFabServer = require('./Scripts/PlayFab/PlayFabServer.js');
