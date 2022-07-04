08f911f198ff672a8b36664a298b5472332a92e4
PlayFab SDK Team <PlayFabSdkAccess@microsoft.com>
2022-07-04 20:17:03 +0000

    https://docs.microsoft.com/en-us/gaming/playfab/release-notes/#220704

--- PlayFabSdk/Scripts/PlayFab/PlayFab.js
+++ PlayFabSdk/Scripts/PlayFab/PlayFab.js
@@ -4,7 +4,7 @@ var url = require("url");
 var https = require("https");
 
 exports.sdk_version = "2.99.220704";
-exports.buildIdentifier = "adobuild_nodesdk_116";
+exports.buildIdentifier = "adobuild_nodesdk_114";
 
 var settings = (exports.settings = {
     productionUrl: ".playfabapi.com",
