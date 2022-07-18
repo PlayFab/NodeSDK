6a04f21f64b747ac38b30bcac585e2134d3b70aa
PlayFab SDK Team <PlayFabSdkAccess@microsoft.com>
2022-07-18 21:55:38 +0000

    https://docs.microsoft.com/en-us/gaming/playfab/release-notes/#220718

--- PlayFabSdk/Scripts/PlayFab/PlayFabServer.js
+++ PlayFabSdk/Scripts/PlayFab/PlayFabServer.js
@@ -820,6 +820,23 @@ exports.GetPlayFabIDsFromSteamIDs = function (request, callback) {
     );
 };
 
+exports.GetPlayFabIDsFromTwitchIDs = function (request, callback) {
+    if (PlayFab.settings.developerSecretKey == null) {
+        throw "Must have PlayFab.settings.DeveloperSecretKey set to call this method";
+    }
+    PlayFab.MakeRequest(
+        PlayFab.GetServerUrl() + "/Server/GetPlayFabIDsFromTwitchIDs",
+        request,
+        "X-SecretKey",
+        PlayFab.settings.developerSecretKey,
+        function (error, result) {
+            if (callback != null) {
+                callback(error, result);
+            }
+        },
+    );
+};
+
 exports.GetPlayFabIDsFromXboxLiveIDs = function (request, callback) {
     if (PlayFab.settings.developerSecretKey == null) {
         throw "Must have PlayFab.settings.DeveloperSecretKey set to call this method";
