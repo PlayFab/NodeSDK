5b43a34a80c54efd852e4372ce7e18fc716f2bb5
PlayFab SDK Team <PlayFabSdkAccess@microsoft.com>
2026-04-13 21:36:43 +0000

    https://docs.microsoft.com/en-us/gaming/playfab/release-notes/#260410

--- PlayFabSdk/Scripts/PlayFab/PlayFab.js
+++ PlayFabSdk/Scripts/PlayFab/PlayFab.js
@@ -3,7 +3,7 @@
 var url = require("url");
 var https = require("https");
 
-exports.sdk_version = "2.194.260327";
+exports.sdk_version = "2.195.260410";
 exports.buildIdentifier = "adobuild_nodesdk_115";
 
 var settings = (exports.settings = {
@@ -19,7 +19,7 @@ var _internalSettings = (exports._internalSettings = {
     entityToken: null,
     sessionTicket: null,
     requestGetParams: {
-        sdk: "JavaScriptSDK-2.194.260327",
+        sdk: "JavaScriptSDK-2.195.260410",
     },
 });
 
